a8915d0ace8d2696f93e8cc0c7b92e8becac10cd
장강민 <jkm9780533@gmail.com>
2026-06-17 16:08:40 +0900

    feat: DTO 설정

--- school/src/main/java/com/dgsw/school/dto/EnrollDTO.java
+++ school/src/main/java/com/dgsw/school/dto/EnrollDTO.java
@@ -1,4 +1,13 @@
 package com.dgsw.school.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class EnrollDTO {
+    private String schoolId;
+    private String subject;
 }
--- school/src/main/java/com/dgsw/school/dto/ResponseDTO.java
+++ school/src/main/java/com/dgsw/school/dto/ResponseDTO.java
@@ -1,4 +1,12 @@
 package com.dgsw.school.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ResponseDTO {
+    private String message;
 }
